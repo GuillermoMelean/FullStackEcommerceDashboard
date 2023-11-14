b8dcaeb7418e81f42e69bff7124ca6c50dbfe84d
Guillermo Melean <guial@live.com.pt>
2023-11-14 01:35:10 +0000

    Add modal component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,33 @@
-import { Button } from "@/components/ui/button"
+'use client'
+
+import { useEffect } from "react";
 import { UserButton } from "@clerk/nextjs";
 
-const SetupPage = () =>{
+import { useStoreModal } from "@/hooks/use-store-modal";
+
+const SetupPage = () => {
+  /*
+    you could use like this
+    const storeModal = useStoreModal();
+
+    but this would not work inside and useEffect,
+    so that's why we extrack the props from the 
+  */
+  const isOpen = useStoreModal((state) => state.isOpen)
+  const onOpen = useStoreModal((state) => state.onOpen)
+
+  useEffect(()=>{
+    if(!isOpen){
+      onOpen();
+    }
+  }, [isOpen,onOpen])
+
   return (
     <div className="p-4">
-      <UserButton afterSignOutUrl="/"/>
+      <UserButton afterSignOutUrl="/" />
+      Root Page
     </div>
   )
 }
-  
+
 export default SetupPage;
