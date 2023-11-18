ab5fc6b6eb2b1e1f729fae78b64ae22e55243ed3
Guillermo Melean <guial@live.com.pt>
2023-11-18 00:47:24 +0000

    Resolve bug with close store modal after create first store

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,28 +1,21 @@
-'use client'
+"use client";
 
 import { useEffect } from "react";
-import { UserButton } from "@clerk/nextjs";
+import { useParams } from "next/navigation";
 
 import { useStoreModal } from "@/hooks/use-store-modal";
 
 const SetupPage = () => {
-  /*
-    you could use like this
-    const storeModal = useStoreModal();
-
-    but this would not work inside and useEffect,
-    so that's why we extrack the props from the 
-  */
- const onOpen = useStoreModal((state) => state.onOpen)
- const isOpen = useStoreModal((state) => state.isOpen)
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
 
   useEffect(() => {
     if (!isOpen) {
       onOpen();
     }
-  }, [isOpen, onOpen])
+  }, [isOpen, onOpen]);
 
   return null;
-}
-
+};
+ 
 export default SetupPage;
