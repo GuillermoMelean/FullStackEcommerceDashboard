499c473f4b28377a68a94d9a4c516a607bc35057
Guillermo Melean <guial@live.com.pt>
2023-12-17 03:45:46 +0000

    Overview

--- app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
@@ -15,7 +15,7 @@ export const OrderClient: React.FC<OrderClientProps> = ({
 }) => {
   return (
     <>
-      <Heading title={`Orders (${data.length})`} description="Manage billboards for your store" />
+      <Heading title={`Orders (${data.length})`} description="Manage orders for your store" />
       <Separator />
       <DataTable searchKey="products" columns={columns} data={data} />
     </>
